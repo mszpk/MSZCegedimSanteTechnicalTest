9eabc5328ed0fe9bade672119b62401f2c461f00
msz <marc.szpeker@gmail.com>
2025-03-21 12:01:05 +0100

    Ajout des méthodes findAll et findByCriteria

--- src/main/java/com/maiia/pro/repository/AvailabilityRepository.java
+++ src/main/java/com/maiia/pro/repository/AvailabilityRepository.java
@@ -1,12 +1,21 @@
 package com.maiia.pro.repository;
 
-import com.maiia.pro.entity.Availability;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.maiia.pro.entity.Availability;
 
 @Repository
 public interface AvailabilityRepository extends CrudRepository<Availability, String> {
-    List<Availability> findByPractitionerId(Integer id);
+    List<Availability> findByPractitionerId(Integer practitionerId);
+
+    List<Availability> findAll();
+    
+    @Query("from Availability av where av.practitionerId = ?1 and (av.patientId is null or av.patientID = ?2) and av.startDate >= ?3 and av.endDate <= ?4 order by av.startDate")
+    List<Availability> findByCriteria(Integer practitionerId, Integer patientId, LocalDateTime startDate, LocalDateTime endDateTime);
+
 }
