cc9c61444edbeaa50a216d231f5ff3e5cf242240
msz <marc.szpeker@gmail.com>
2025-03-21 11:58:05 +0100

    Ajout de la méthode addAppointment

--- src/main/java/com/maiia/pro/controller/ProAppointmentController.java
+++ src/main/java/com/maiia/pro/controller/ProAppointmentController.java
@@ -1,10 +1,16 @@
 package com.maiia.pro.controller;
 
+import com.maiia.pro.dto.AppointmentDTO;
 import com.maiia.pro.entity.Appointment;
 import com.maiia.pro.service.ProAppointmentService;
+import com.maiia.pro.service.ProAvailabilityService;
+
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,8 +19,12 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/appointments", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProAppointmentController {
+	
     @Autowired
     private ProAppointmentService proAppointmentService;
+
+    @Autowired
+    private ProAvailabilityService proAvailabilityService;
 
     @ApiOperation(value = "Get appointments by practitionerId")
     @GetMapping("/{practitionerId}")
@@ -26,5 +36,14 @@ public class ProAppointmentController {
     @GetMapping
     public List<Appointment> getAppointments() {
         return proAppointmentService.findAll();
+    }
+    
+    @PostMapping("/appointment")
+    public ResponseEntity<Object> addAppointment(@RequestBody AppointmentDTO dto) {
+    	if (proAvailabilityService.checkPractitionnerAvailability(dto.getPractitionerId(), dto.getStartDate(), dto.getEndDate())) {
+    		proAppointmentService.save(dto.getPatientId(), dto.getPractitionerId(), dto.getStartDate(), dto.getEndDate());
+    		return ResponseEntity.ok().body(dto);
+    	}
+    	return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Créneau horaire indisponible");
     }
 }
