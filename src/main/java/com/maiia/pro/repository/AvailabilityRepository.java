cfe551941dd6a8ba85a0e184ccf918b3ea236d9a
msz <marc.szpeker@gmail.com>
2025-03-21 17:02:36 +0100

    Ajout de la méthode deleteByCriteria

--- src/main/java/com/maiia/pro/repository/AvailabilityRepository.java
+++ src/main/java/com/maiia/pro/repository/AvailabilityRepository.java
@@ -3,6 +3,7 @@ package com.maiia.pro.repository;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -13,9 +14,10 @@ import com.maiia.pro.entity.Availability;
 public interface AvailabilityRepository extends CrudRepository<Availability, String> {
     List<Availability> findByPractitionerId(Integer practitionerId);
 
-    List<Availability> findAll();
-    
-    @Query("from Availability av where av.practitionerId = ?1 and (av.patientId is null or av.patientID = ?2) and av.startDate >= ?3 and av.endDate <= ?4 order by av.startDate")
-    List<Availability> findByCriteria(Integer practitionerId, Integer patientId, LocalDateTime startDate, LocalDateTime endDateTime);
+    @Query("from Availability av where av.practitionerId = ?1 and av.startDate >= ?2 and av.endDate <= ?3 order by av.startDate")
+    List<Availability> findByCriteria(Integer practitionerId, LocalDateTime startDate, LocalDateTime endDateTime);
 
+    @Modifying
+    @Query("delete from Availability av where av.practitionerId = ?1 and av.startDate >= ?2 and av.endDate <= ?3")
+    void deleteByCriteria(Integer practitionerId, LocalDateTime startDate, LocalDateTime endDateTime);
 }
