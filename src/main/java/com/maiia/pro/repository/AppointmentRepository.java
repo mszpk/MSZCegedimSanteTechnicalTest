9f791e61465a107398dbaecdd02e425f454b5464
msz <marc.szpeker@gmail.com>
2025-03-21 12:00:11 +0100

    Ajout de la méthode save

--- src/main/java/com/maiia/pro/repository/AppointmentRepository.java
+++ src/main/java/com/maiia/pro/repository/AppointmentRepository.java
@@ -1,14 +1,17 @@
 package com.maiia.pro.repository;
 
-import com.maiia.pro.entity.Appointment;
-import com.maiia.pro.entity.Patient;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.maiia.pro.entity.Appointment;
 
 @Repository
 public interface AppointmentRepository extends CrudRepository<Appointment, String> {
     List<Appointment> findByPractitionerId(Integer practitionerId);
     List<Appointment> findAll();
+    @SuppressWarnings("unchecked")
+	Appointment save(Appointment appointment);
+    
 }
